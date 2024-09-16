ae2241bf9d91a1909b74af98d88497cc51ae0ec2
Javier Prado <javier.orlando@coara.co>
2024-09-16 17:16:13 +0200

    Add groups

--- player.py
+++ player.py
@@ -4,6 +4,8 @@ from circleshape import CircleShape
 
 
 class Player(CircleShape):
+    containers = ()
+    
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
